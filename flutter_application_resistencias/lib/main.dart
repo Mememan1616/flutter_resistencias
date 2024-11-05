04b2e66224509ef821b47e0562814212fbccd9bd
Mememan1616 <pm0l000054@gmail.com>
2024-11-05 11:46:38 -0600

    Update main.dart

--- flutter_application_resistencias/lib/main.dart
+++ flutter_application_resistencias/lib/main.dart
@@ -5,6 +5,7 @@ int r1=0;
 int r2=0;
 int r3=0;
 double r4=0.0;
+final int rcolor=0;
 String resultado="";
  var calcular=new calcula_resistencias();
 void main() {
@@ -41,7 +42,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  
+  double? _selectedValue;
   Widget build(BuildContext context) {
    
     
@@ -68,25 +69,27 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     }
 
-    void dropdownCallback4(double? selectedValue){
-      if( selectedValue is double){
+    void dropdownCallback4(double? ts){
+      if( ts is double){
         setState(() {
-          r4=selectedValue;
+          r4=ts;
         });
       }
     }
     
     return Scaffold(
       appBar: AppBar(
-        
+      
       backgroundColor: Colors.amber,
       title: Text(widget.title),
       
       ),
       body: Center(
+        
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
+          
           children: <Widget>[
 
             
@@ -111,7 +114,25 @@ class _MyHomePageState extends State<MyHomePage> {
                  onChanged:dropdownCallback1,
 
                  ),
-                 Text(" El valor es ${r1}")
+                 Container(
+                  color: r1==0 ? Colors.black : 
+                  r1==1? Colors.brown: 
+                  r1==2? Colors.red:
+                  r1==3? Colors.orange:
+                  r1==4? Colors.amber:
+                  r1==5? Colors.green:
+                  r1==6? Colors.blueAccent:
+                  r1==7? Colors.purple:
+                  r1==8? Colors.grey:
+                  Colors.white
+                  , 
+                 child:Text(" El valor es ${r1}",style: TextStyle(
+                  fontSize: 20,
+                  color: r1>8? Colors.black: Colors.white 
+                 ), 
+                 
+                
+                 ))
                 
                 
                ],
@@ -137,7 +158,25 @@ class _MyHomePageState extends State<MyHomePage> {
                  onChanged:dropdownCallback2,
 
                  ),
-                 Text(" El valor es ${r2}")
+                 Container(
+                  color: r2==0 ? Colors.black : 
+                  r2==1? Colors.brown: 
+                  r2==2? Colors.red:
+                  r2==3? Colors.orange:
+                  r2==4? Colors.amber:
+                  r2==5? Colors.green:
+                  r2==6? Colors.blueAccent:
+                  r2==7? Colors.purple:
+                  r2==8? Colors.grey:
+                  Colors.white
+                  , 
+                 child:Text(" El valor es ${r2}",style: TextStyle(
+                  fontSize: 20,
+                  color: r2>8? Colors.black: Colors.white 
+                 ), 
+                 
+                
+                 ))
                 
                 
                ],
@@ -148,7 +187,8 @@ class _MyHomePageState extends State<MyHomePage> {
                
                children: <Widget>[
                   DropdownButton(items:const [
-                  DropdownMenuItem<int>(value:0,child: Text("Negro"),),
+                  DropdownMenuItem<int>(value:0,child: Text(""),),
+                  DropdownMenuItem<int>(value:1,child: Text("Negro"),),
                   DropdownMenuItem<int>(value:10,child: Text("Cafe"),),
                   DropdownMenuItem<int>(value:100,child: Text("Rojo"),),
                   DropdownMenuItem<int>(value:1000,child: Text("Naranja"),),
@@ -164,33 +204,67 @@ class _MyHomePageState extends State<MyHomePage> {
                  onChanged:dropdownCallback3,
 
                  ),
-                 Text(" El valor es ${r3}")
+                 Container(
+                  color: r3==0 ? Colors.black :
+                  r3==1 ? Colors.black:
+                  r3==10? Colors.brown: 
+                  r3==100? Colors.red:
+                  r3==1000? Colors.orange:
+                  r3==10000? Colors.amber:
+                  r3==100000? Colors.green:
+                  r3==1000000? Colors.blueAccent:
+                  r3==10000000? Colors.purple:
+                  r3==100000000? Colors.grey:
+                  Colors.white
+                  , 
+                 child:Text(" El valor es ${r3}",style: TextStyle(
+                  fontSize: 20,
+                  color: r3>8? Colors.black: Colors.white 
+                 ), 
+                 
+                
+                 ))
                 
                 
                ],
             ),
 
 
-           /* Row(
+             Row(
                
                children: <Widget>[
                   DropdownButton(items:const [
-                  DropdownMenuItem<double>(value:0.1,child: Text("Dorado"),),
-                  DropdownMenuItem<double>(value:0.05,child: Text("Plateado"),),
+                  DropdownMenuItem<double>(value:0,child: Text(""),),
+                  DropdownMenuItem<double>(value:0.1,child: Text("Plateado"),),
+                  DropdownMenuItem<double>(value:0.05,child: Text("Dorado"),),
                   
-
                  ], 
                  value: r4,
-                 onChanged:dropdownCallback4,
+                 onChanged: dropdownCallback4,
 
                  ),
-                 Text(" El valor es ${r4}")
+                 Container(
+                  color: r4==0 ? Colors.white : 
+                  r4==0.1? Colors.grey[400]: 
+                  
+                  Colors.amberAccent
+                  , 
+                 child:Text(" El valor es ${r4}",style: TextStyle(
+                  fontSize: 20,
+                  color: r2>8? Colors.black: Colors.white 
+                 ), 
+                 
+                
+                 ))
                 
                 
                ],
-            ),*/
+            ),
 
-            
+
+           
+
+          SizedBox(height: 30,width:30,),
            ElevatedButton(
                 //The style of the button
                 style: ElevatedButton.styleFrom(
@@ -198,7 +272,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   backgroundColor: const Color.fromARGB(255, 248, 4, 4),
                 ),
                 //The text and style of the button 
-                child: Text("CALCULAR RESISTENCIA", style: TextStyle(color: Colors.white)),
+                child: Text("CALCULAR RESISTENCIA", style: TextStyle(color: Colors.white, fontFamily:"Arial", fontSize:20)),
                 onPressed: (){
                 setState(() {
                   
@@ -206,7 +280,8 @@ class _MyHomePageState extends State<MyHomePage> {
                  calcular.c1="${r1}";
                  calcular.c2="${r2}";
                  calcular.c3=r3;
-                 calcular.calcula(calcular.c1,calcular.c2,calcular.c3,0.05);
+                 calcular.tolerancia=r4;
+                 calcular.calcula(calcular.c1,calcular.c2,calcular.c3,calcular.tolerancia);
                 });
                  
                  //resultado="El total es: ${calcular.total}, el minimo ${calcular.min}, y el maximo es ${calcular.max}";
@@ -215,7 +290,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
                 },
               ),
-
+               SizedBox(height: 30,width:30,),
               Row(children: [
                   Text("El valor de las resistencias es: ", style:TextStyle(color: Colors.black87, fontSize:20) ),
                   
